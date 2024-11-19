b325544101bc9b75e531843ffbc5621bc743358c
Artag/Chris <scristxyz@gmail.com>
2024-11-19 17:01:37 -0500

    act

--- src/orders/dto/index.ts
+++ src/orders/dto/index.ts
@@ -2,3 +2,5 @@ export * from './create-order.dto';
 export * from './order-pagination.dto';
 export * from './update-order.dto';
 export * from './status.dto';
+export * from './order-item.dto';
+export * from './order-pagination.dto';
